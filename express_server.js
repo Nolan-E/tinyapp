46b2fcc0287ec4c70ead729bb21e80e415cb2dff
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-15 14:29:19 -0700

    Added /urls.json route to server.

--- express_server.js
+++ express_server.js
@@ -3,16 +3,23 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-// SERVER CODE
+// IMITATION DATABASE OBJECT
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// SERVER ROUTING
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
 
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+
+// PORT LISTENER
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
