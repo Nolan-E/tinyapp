26e2298213b0da45c8ca74d350160fd5780e8531
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-15 19:33:08 -0700

    Updated POST for new URL submit to generate a random string and save the string as a new shortURL & it's associated longURL in the urlDatabase object. Also added a redirect to new shortURL page.

--- express_server.js
+++ express_server.js
@@ -30,8 +30,10 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send(generateRandomString());
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
